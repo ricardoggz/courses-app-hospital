395e337916867eac12c6be26b185aeaf21e83f16
ricardoggz <ricardo_ggzicm@outlook.com>
2023-01-05 15:07:04 -0600

    Estilos extendibles por props en el componente button

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,9 +1,20 @@
 import styled from "styled-components"
 
 export const ButtonWrapper = styled.button`
-    background:var(--color-blue-secondary);
+    background: ${(props)=>{
+        switch(true){
+            default : return`
+                var(--color-blue-primary);
+                color:var(--color-light-primary);
+            `
+            case props.blueSecondary : return`
+                var(--color-blue-secondary);
+                color: var(--color-blue-primary);
+            `
+        }
+    }};
+    cursor:pointer;
     border:none;
-    color:var(--color-blue-primary);
     padding: 1rem;
     width:100%;
 `
