a745825c2b302578f51d9501ce1462c104afcb06
ricardoggz <ricardo_ggzicm@outlook.com>
2023-01-06 15:39:41 -0600

    Nueva ruta al dashboard

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
-import { LoginView, HomeView } from '../views'
+import { LoginView, HomeView, DashboardUserView } from '../views'
 
 export const AppRoutes = ()=>{
     return (
         <Routes>
             <Route path='/' element={<HomeView />}/>
             <Route path='/login' element={<LoginView />} />
+            <Route path='/dashboard' element={<DashboardUserView />} />
         </Routes>
     )
 }
