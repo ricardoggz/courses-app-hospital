f3eed8c7eba1cc8b4007fe88bde96b2ec146a12f
ricardoggz <ricardo_ggzicm@outlook.com>
2023-01-09 08:12:23 -0600

    endpoint de login cambiado

--- src/services/login/login.js
+++ src/services/login/login.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const login= async(data)=>{
     try {
-        const response = await axios.post(`http://localhost:3030/login-user`, data)
+        const response = await axios.post(`http://localhost:3030/api/auth/login-user`, data)
         console.log(response)
     } catch (error) {
         throw new Error(error)
