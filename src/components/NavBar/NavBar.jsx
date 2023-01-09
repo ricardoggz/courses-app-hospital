1c2c1230e89049daf8893c67a07942cf65db3a47
ricardoggz <ricardo_ggzicm@outlook.com>
2023-01-09 14:02:00 -0600

    Navbar responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Header, Nav, Ul } from './NavBar.styled'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { Header, Nav, Ul, NavButton } from './NavBar.styled'
 import logo from '../../assets/logo.png'
 
 export const NavBar = ()=>{
+    const [isOpen, setIsOpen] = useState(false)
+    const showMenu = ()=>setIsOpen(!isOpen)
     return (
         <Header>
             <Nav className='flex-container'>
                 <div><img src={logo} /></div>
-                <Ul>
-                    <li><Link to='/'>Inicio</Link></li>
-                    <li><Link to='/login'>Iniciar sesiòn</Link></li>
+                <NavButton onClick={showMenu}>
+                    { !isOpen ? <FaBars /> : <FaTimes /> }
+                </NavButton>
+                <Ul open={isOpen}>
+                    <li><Link to='/' onClick={showMenu}>Inicio</Link></li>
+                    <li><Link to='/login' onClick={showMenu}>Iniciar sesiòn</Link></li>
                 </Ul>
             </Nav>
         </Header>
--- src/components/NavBar/NavBar.styled.js
+++ src/components/NavBar/NavBar.styled.js
@@ -23,4 +23,39 @@ export const Ul = styled.ul`
         color:var(--color-light-primary);
         text-decoration:none;
     }
+
+    @media (min-width:240px) and (max-width:880px){
+        background: var(--color-blue-primary);
+        gap: 2rem;
+        left: 0;
+        right: 0;
+        position: absolute;
+        top: 4rem;
+        width: 100%;
+        height: 100vh;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        transition: 500ms all ease-in-out;
+        transform: translateX(${({ open }) => (open ? "0" : "-100%")});
+        z-index: 100;
+        li{
+        width:90%;
+        border-bottom:1px solid var(--color-light-primary);
+        text-align:center;
+        padding: 1rem 0;
+        }
+    }
+`
+export const NavButton = styled.button`
+    background:none;
+    border:none;
+    color:var(--color-light-primary);
+    font-size:2rem;
+    display:none;
+
+    @media (min-width: 240px) and (max-width:880px){
+        display:block;
+    }
+
 `
