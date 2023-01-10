883a550fde67cc9af6661c5654176c3012bd47de
ricardoggz <ricardo_ggzicm@outlook.com>
2023-01-10 09:27:59 -0600

    Nuevo componente footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import { FooterWrapper } from "./Footer.styled"
+export const Footer = ()=>{
+    return (
+        <FooterWrapper>
+            <span>
+                &copy; Todos los derechos reservados - Hospital Infantil de Mèxico Federico Gòmez
+            </span>
+        </FooterWrapper>
+    )
+}
---
+++ src/components/Footer/Footer.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const FooterWrapper = styled.footer`
+    background:var(--color-dark-primary);
+    color:var(--color-light-primary);
+    padding: 1rem 0;
+    text-align:center;
+    width:100%;
+`
