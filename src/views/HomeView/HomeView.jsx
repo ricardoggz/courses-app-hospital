25a27bc271e62615b5c5e7864ac33f0bbcc5e437
ricardoggz <ricardo_ggzicm@outlook.com>
2023-01-25 12:52:03 -0600

    Filtrado de arreglo creado

--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import {
     Container,
     Card,
@@ -10,6 +12,12 @@ import {
 import { useFetch } from '../../hooks'
 
 export const HomeView = ()=>{
+    let months = [
+        {id: 1, name: 'Enero'},{id: 2, name : 'Febrero'},
+        {id: 3, name: 'Marzo'},{id: 4, name: 'Abril'},
+        {id: 5, name: 'Mayo'},{id:6, name: 'Junio'}
+    ];
+    const [selectedTab, setSelectedTab] = useState(months[0]);
     const config = {
         headers: { Authorization: `Bearer 586637bf90ea7727edc8c90c95b056c3` }
     };
@@ -17,18 +25,31 @@ export const HomeView = ()=>{
         //url: 'https://api.vimeo.com/me/projects/14457817/videos',
         url: `${import.meta.env.VITE_BASE_URL_API}api/courses/all-courses`
     })
+    let filteredData;
+    if(!loading) filteredData = data.data.filter(course => course.month_id === selectedTab.id)
+    
     return (
             <>
                 <Jumbotron />
                 <Container>
                  <Title>Nuestra oferta educativa - 2023</Title>
+                 <ul>
+                    {months.map(month=>(
+                        <li
+                            key={month.id}
+                            onClick={()=>setSelectedTab(month)}
+                        >
+                            {month.name}
+                        </li>
+                    ))}
+                 </ul>
                 {
                     loading?
                     <Spinner />
                     :
                     <GridContainer>
                         {
-                            data.data.map((course, index)=>(
+                            filteredData.map((course, index)=>(
                                 <Card
                                     key={index}
                                     name={course.course_name}
