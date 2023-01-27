9bf88bf49dec4bc63e545f516375019ac01ee4be
ricardoggz <ricardo_ggzicm@outlook.com>
2023-01-27 10:45:30 -0600

    Footer agregado en home view

--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -59,7 +59,7 @@ export const HomeView = ()=>{
                     </>
                 }
                 </Container>
-                {/*<Footer />*/}
+                <Footer />
             </>
     )
 }
