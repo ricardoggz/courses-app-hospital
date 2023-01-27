8e382ea29099947f69991840c9f13557d9464663
ricardoggz <ricardo_ggzicm@outlook.com>
2023-01-27 09:28:26 -0600

    responsive de grid container cambiado

--- src/components/GridContainer/GridContainer.styled.js
+++ src/components/GridContainer/GridContainer.styled.js
@@ -5,7 +5,10 @@ export const GridWrapper = styled.div`
     gap:1rem;
     grid-template-columns: repeat(3,1fr);
     
-    @media (min-width:240px) and (max-width:880px){
+    @media (min-width:240px) and (max-width:900px){
         grid-template-columns: 1fr;
+    }
+    @media (min-width:900px) and (max-width:1440px){
+        grid-template-columns: repeat(2,1fr);
     }
 `
